142152c453a70d58672f8573a6387ba4727b8900
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-03-11 00:04:39 +0530

    Leetcode Problems

---
+++ LeetCode/ProductofArray.java
@@ -0,0 +1,33 @@
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int n = nums.length;
+//         int res[] = new int[n];
+//         Arrays.fill(res,1);
+//          for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i!=j) {
+//                     res[i] *=nums[j];
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        // int suffix[] = new int[n];
+        Arrays.fill(ans,1);
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1]*nums[i-1];
+        }
+        int suffix = 1;
+        for(int i=n-2;i>=0;i--){
+            suffix *= nums[i+1];
+            ans[i] *=suffix;
+        }
+        return ans;
+    }
+}
