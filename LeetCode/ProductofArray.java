735197b01e84a7d738564eeb53ca057fdff380bc
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-03-11 00:11:50 +0530

    Leetcode Problems

--- LeetCode/ProductofArray.java
+++ LeetCode/ProductofArray.java
@@ -19,7 +19,6 @@ class Solution {
         int n = nums.length;
         int ans[] = new int[n];
         // int suffix[] = new int[n];
-        Arrays.fill(ans,1);
         for(int i=1;i<n;i++){
             ans[i]=ans[i-1]*nums[i-1];
         }
