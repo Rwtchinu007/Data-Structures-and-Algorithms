8753904e0cc144b2afe233cbe6da278f62715ca6
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-07-06 11:45:06 +0530

    Leetcode Problems

---
+++ LeetCode/BinaryTreeRightSideView.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static void solve(TreeNode root,int level,List<Integer> ans){
+        if(root==null) return;
+        if(level == ans.size()) ans.add(root.val);
+        solve(root.right,level+1,ans);
+        solve(root.left,level+1,ans);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        solve(root,0,ans);
+        return ans;
+        
+    }
+}
