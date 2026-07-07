d97f58f155da1e23919b143faac071f1a9faa60c
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-07-07 19:35:18 +0530

    Leetcode Problems

---
+++ LeetCode/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null)
+            return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+            boolean leftToRight = true;
+        while (q.size() != 0) {
+            int size = q.size();
+            List<Integer> curr = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = q.poll();
+                curr.add(temp.val);
+                if (temp.left != null)
+                    q.offer(temp.left);
+                if (temp.right != null)
+                    q.offer(temp.right);
+            }
+            if(leftToRight){
+            ans.add(curr);
+            }
+            else{
+                Collections.reverse(curr);
+                ans.add(curr);
+            }
+            leftToRight = !leftToRight;
+        }
+        return ans;
+
+    }
+}
