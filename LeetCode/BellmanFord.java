7d6aa417fc525fa679812a19f6c0b5ecae0fbac8
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-08-18 21:29:39 +0530

    Bellman Ford

---
+++ LeetCode/BellmanFord.java
@@ -0,0 +1,33 @@
+class Solution {
+	public ArrayList<Integer> bellmanFord(int V, int[][] edges, int src) {
+		int dist[] = new int[V];
+		Arrays.fill(dist, 100000000);
+		dist[src] = 0;
+		for (int i = 0; i<V - 1; i++) {
+			for (int[]edge:edges) {
+				int u = edge[0];
+				int v = edge[1];
+				int w = edge[2];
+				if (dist[u] != 100000000 && dist[u]+w<dist[v]) {
+					dist[v] = dist[u]+w;
+				}
+			}
+		}
+		for (int[]edge:edges) {
+			int u = edge[0];
+			int v = edge[1];
+			int w = edge[2];
+			if (dist[u] != 100000000 && dist[u]+w<dist[v]) {
+				ArrayList<Integer> ans = new ArrayList<>();
+				ans.add(-1);
+				return ans;
+			}
+		}
+		ArrayList<Integer> ans = new ArrayList<>();
+		for (int num:dist) {
+			ans.add(num);
+		}
+		return ans;
+		
+	}
+}
